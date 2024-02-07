f785bdc532b7c4ecab928d02e7d48108fec62ea3
Julien Lavigne <julavius@hotmail.com>
2024-02-07 13:17:41 -0500

    debut partie 1.1

--- INF2610_TP3/INF2610TP3_01L/part1.c
+++ INF2610_TP3/INF2610TP3_01L/part1.c
@@ -15,6 +15,38 @@
 
 void question1()
 {
-   
+    int parent1;
+    int parent2;
+    int parent3;
+   if((parent1 = fork()) ==0){
+    // 1.1
+    if (fork() == 0){
+        // 2.1
+    }
+    if (fork() == 0) {
+        // 2.2
+    }
+
+   }
+   if((parent2 = fork()) == 0){
+    // 1.2
+    if (fork() ==0 ) {
+        // 2.3
+    }
+   }
+   if((parent3 = fork()) == 0){}
     // TODO
+    // 1.3
+    if (fork() == 0) {
+        // 2.4
+    }
+    if( fork() == 0){
+        // 2.5
+    }
+    if( fork() == 0){
+        // 2.6
+    }
+
+    while(wait(NULL)>0);
+    _exit(0);
 }
