0462ab2c314864bc46c6e7f3305f40cb6333cbf8
Julien Lavigne <julavius@hotmail.com>
2024-02-08 14:31:16 -0500

    Partie 2 fini

--- INF2610_TP3/INF2610TP3_01L/part2.c
+++ INF2610_TP3/INF2610TP3_01L/part2.c
@@ -28,13 +28,12 @@ void* contribution(void*p)
 {
     // TODO
   long tempVal = 0;
-  printf("Valeur de p: %ld \n", (long)p);
-  for (int i = ((long)p * m / nb) + 1; i < ((long)p + 1)  * m / nb; i++) {
+  printf("Valeur de p: %d \n", *(int*)p);
+  for (int i = ((*(int*)p) * m / nb) + 1; i <= ((*(int*)p + 1) * m / nb); i++) {
     tempVal += i;
   }
-  somme[(long)p] = tempVal;
-  pthread_exit(NULL);
-  return NULL;
+  somme[*(int*)p] = tempVal;
+    pthread_exit(NULL);
 }
 
 
@@ -42,23 +41,28 @@ void question2( )
 {
     // TODO
     pthread_t tid[nb];
-  for(int i = 0 ; i< nb; i++){
-      pthread_create(&tid[i], NULL, contribution, &i);
+    int thread_args[nb];
 
-  }
-    
-  for(int i = 0 ; i< nb; i++){
-    pthread_join(tid[i], NULL);
+    for(int i = 0 ; i< nb; i++){
+      thread_args[i] = i;
+      pthread_create(&tid[i], NULL, contribution, &thread_args[i]);
   }
 
+  for (int j = 0; j < nb; j++) {
+      printf("Ceci est le résultat du join:  %d, thread numéro %d \n",pthread_join(tid[j], NULL), j );
+  }
 
-  int sommeTotale = 0;
+  printf("BAJOUR \n");
+
+  long sommeTotale = 0;
   for (int i = 0; i < nb; i++){
     sommeTotale += somme[i];
   }
-  printf("Somme avec threads: %d", sommeTotale);
+  printf("Somme avec threads: %ld \n", sommeTotale);
 
-  int sommeConnue = ( m + 1 ) / 2;
-  printf("Somme connue: %d", sommeConnue);
+  long sommeConnue = (long)m * ( (long)m + 1 ) / 2;
+  printf("Somme connue: %ld \n", sommeConnue);
+
+
  _exit(0);
 }
