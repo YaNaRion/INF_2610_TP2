3e54330be4a681f21be5daffe0e82d2e394bc488
Julien Lavigne <julavius@hotmail.com>
2024-03-12 20:59:24 -0400

    wahu finitto

--- TP4_01L/TubesAnonymes.c
+++ TP4_01L/TubesAnonymes.c
@@ -14,34 +14,37 @@ int main() {
      if(fork() == 0) {
         int secondPipe[2];  
         pipe(secondPipe);
+
         if (fork() == 0) {  // P2 tr [a-z] [A-Z]
             int firstPipe[2];
             pipe(firstPipe);
-
+            
             if (fork() == 0) { // P1  cat In.txt
-                dup2(firstPipe[1], 1);
+                dup2(firstPipe[1], STDOUT_FILENO);
                 close(firstPipe[0]);
                 close(firstPipe[1]);
                 execlp("cat", "cat", "In.txt", NULL);
-                _exit(1);
             } 
-            dup2(firstPipe[0], 0);
-            dup2(secondPipe[1], 1);
+            dup2(firstPipe[0], STDIN_FILENO);
+            dup2(secondPipe[1], STDOUT_FILENO);
             close(firstPipe[0]);
             close(firstPipe[1]);
             close(secondPipe[0]);
             close(secondPipe[1]);
             execlp("tr", "tr", "[a-z]", "[A-Z]", NULL);
-            _exit(1);
         }   
 
-        int outTxt = open("Out.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
-        dup2(outTxt, 1);
-        dup2(secondPipe[0], 0);
+        // rediriger la sortie de Diff (sortie standard) vers le fichier Out.txt
+        int outTxt = open("Out.txt", O_WRONLY | O_CREAT| O_TRUNC, 0600);
+        dup2(outTxt, STDOUT_FILENO);
+        close(outTxt);
+
+        // remplace le "-" (entrée standard) par l'entrée du deuxième Pipe
+        dup2(secondPipe[0], STDIN_FILENO);
+
         close(secondPipe[0]);
         close(secondPipe[1]);
         execlp("diff", "diff", "-","In.txt", NULL);
-        _exit(1);
         }
 
     while (wait(NULL) > 0);
