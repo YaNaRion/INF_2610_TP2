c08ba17a018b841543225d2731e072456ef2402f
Julien Lavigne <julavius@hotmail.com>
2024-03-12 21:21:24 -0400

    finito

--- TP4_01L/TubesAnonymes.c
+++ TP4_01L/TubesAnonymes.c
@@ -1,7 +1,4 @@
 // Ajouter les directives d'inclusion nécessaires
-#include <stdio.h> // pour printf
-#include <stdlib.h> // pour rand and srand
-#include <time.h> // pour time
 #include <unistd.h> // pour write, read, pipe and close
 #include <sys/wait.h> // pour wait
 #include <fcntl.h> // pour open
@@ -20,30 +17,41 @@ int main() {
             pipe(firstPipe);
             
             if (fork() == 0) { // P1  cat In.txt
+                // Pour cat, rediriger la sortie standard vers l'entrée du premier Pipe
                 dup2(firstPipe[1], STDOUT_FILENO);
+
+                // Fermer les descripteurs inutiles
                 close(firstPipe[0]);
                 close(firstPipe[1]);
+                
                 execlp("cat", "cat", "In.txt", NULL);
             } 
+            // Pour tr, Rediriger entrée standard à entrée premier Pipe
+            // et sortie standard vers sortie second Pipe
             dup2(firstPipe[0], STDIN_FILENO);
             dup2(secondPipe[1], STDOUT_FILENO);
+
+            // Fermer les descripteurs inutiles
             close(firstPipe[0]);
             close(firstPipe[1]);
             close(secondPipe[0]);
             close(secondPipe[1]);
+
             execlp("tr", "tr", "[a-z]", "[A-Z]", NULL);
         }   
 
         // rediriger la sortie de Diff (sortie standard) vers le fichier Out.txt
         int outTxt = open("Out.txt", O_WRONLY | O_CREAT| O_TRUNC, 0600);
         dup2(outTxt, STDOUT_FILENO);
-        close(outTxt);
 
         // remplace le "-" (entrée standard) par l'entrée du deuxième Pipe
         dup2(secondPipe[0], STDIN_FILENO);
 
+        // Fermer les descripteurs inutiles
+        close(outTxt);
         close(secondPipe[0]);
         close(secondPipe[1]);
+
         execlp("diff", "diff", "-","In.txt", NULL);
         }
 
--- TP4_01L/TubesNommes.c
+++ TP4_01L/TubesNommes.c
@@ -1,5 +1,63 @@
 // Ajouter les directives d'inclusion nécessaires
+#include <unistd.h> // pour write, read, pipe and close
+#include <sys/wait.h> // pour wait
+#include <fcntl.h> // pour open
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
 
 int main() {
-    
+
+    mkfifo("secondPipe", 0666);
+    mkfifo("firstPipe", 0666);
+     // P3 diff - In.txt > Output.txt
+     if(fork() == 0) {
+
+        if (fork() == 0) {  // P2 tr [a-z] [A-Z]
+
+            if (fork() == 0) { // P1  cat In.txt
+                // Rediriger la sortie standard vers l'entrée du premier Pipe
+                int firstWrite = open("firstPipe", O_WRONLY);
+                dup2(firstWrite, STDOUT_FILENO);
+
+                // Fermer le descripteur inutile
+                close(firstWrite);
+
+                execlp("cat", "cat", "In.txt", NULL);
+            } 
+
+            // rediriger la sortie standard vers le second Pipe
+            // et entrée standard vers le premier pipe
+            int secondWrite = open("secondPipe", O_WRONLY); 
+            int firstRead = open("firstPipe", O_RDONLY);
+            dup2(secondWrite, STDOUT_FILENO);
+            dup2(firstRead, STDIN_FILENO);
+            
+            // Fermer les descripteurs inutiles
+            close(secondWrite);
+            close(firstRead);
+
+            execlp("tr", "tr", "[a-z]", "[A-Z]", NULL);
+        }   
+
+        // rediriger la sortie de Diff (sortie standard) vers le fichier Outn.txt
+        int outTxt = open("Outn.txt", O_WRONLY | O_CREAT| O_TRUNC, 0600);
+        dup2(outTxt, STDOUT_FILENO);
+
+        // remplace le "-" (entrée standard) par l'entrée du deuxième Pipe
+        int secondRead = open("secondPipe", O_RDONLY);
+        dup2(secondRead, STDIN_FILENO);
+
+        // Fermer les descripteurs inutiles
+        close(secondRead);
+        close(outTxt);
+
+        execlp("diff", "diff", "-","In.txt", NULL);
+        }
+
+    while (wait(NULL) > 0);
+    // Fermer les pipes une fois le programme terminé
+    unlink("firstPipe");
+    unlink("secondPipe");
+    return 0;
 }
